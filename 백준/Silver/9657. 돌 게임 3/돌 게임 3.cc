099ac13a4705ea300394cd7bbf1b800ed391f789
Checking-pks <69112514+Checking-pks@users.noreply.github.com>
2022-08-13 16:51:56 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int num;
+    cin >> num;
+    
+    switch (num%7) {
+        case 0:
+        case 2:
+            cout << "CY";
+            break;
+        case 1:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            cout << "SK";
+            break;
+    }
+}
