ccbeb1e249b32818ab0fdfe4badb0067b40b8770
Checking-pks <69112514+Checking-pks@users.noreply.github.com>
2022-08-13 16:46:14 +0900

    [Silver V] Title: 소수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int min, max, sum = 0, minnum = 10001;
+	cin >> min;
+	cin >> max;
+	
+	int list[10000] = {0,};
+
+	for (int i = 0; i <= max - min; i++) {
+		list[i] = min + i;
+
+		for (int j = 2; j < int(sqrt(list[i])) + 1; j++) {
+			if (list[i] % j == 0) {
+				list[i] = 0;
+				break;
+			}
+		}
+		
+		if (list[i] == 1) {
+			list[i] = 0;
+		}
+		else if (list[i] != 0) {
+			sum += list[i];
+			if (list[i] < minnum) {
+				minnum = list[i];
+			}
+		}
+
+	}
+
+	if (minnum != 10001) {
+		cout << sum << "\n" << minnum;
+	}
+	else {
+		cout << "-1";
+	}
+
+}
