67f96dc2bf26bce77ff29728def37cb82eefab5f
Checking-pks <69112514+Checking-pks@users.noreply.github.com>
2022-08-13 16:54:14 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string nicname;
+    cin >> nicname;
+    
+    cout << nicname << "??!";
+    
+    return 0;
+}
