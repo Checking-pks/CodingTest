f75f8cdd37fb9b6bf6a39d4be0aabe3c03ec78e8
Checking-pks <69112514+Checking-pks@users.noreply.github.com>
2022-08-13 16:51:36 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score;
+    cin >> score;
+    if ((100 >= score) && (score >= 90)) {
+        cout << "A";
+    } else if ((89 >= score) && (score >= 80)) {
+        cout << "B";
+    } else if ((79 >= score) && (score >= 70)) {
+        cout << "C";
+    } else if ((69 >= score) && (score >= 60)) {
+        cout << "D";
+    } else {
+        cout << "F";
+    }
+}
