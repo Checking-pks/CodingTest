20012b66c5cc0eb4bc9a216928de4aa26f9806f8
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2022-10-14 16:19:24 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string nickname;
+    cin >> nickname;
+    
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << nickname << "::fan:\n";
+    cout << ":fan::fan::fan:";
+}
