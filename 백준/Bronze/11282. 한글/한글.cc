0c1f1a6b19b01af3952fed14158a9a5b21855898
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2022-10-28 15:44:35 +0900

    [Bronze IV] Title: 한글, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11282. 한글/한글.cc
+++ 백준/Bronze/11282. 한글/한글.cc
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <codecvt>
 #include <locale>
-#include <string>
 
 using namespace std;
 
 int main() {
-	int num;
-	cin >> num;
-
-	wstring_convert<codecvt_utf8_utf16<wchar_t>> wchar_to_utf8;
-
-	cout << wchar_to_utf8.to_bytes(num + 44031);
+	int n;
+	cin >> n;
+	
+	wstring_convert<codecvt_utf8_utf16<wchar_t>> cvt;
+	cout << cvt.to_bytes(n + 44031);
 }
