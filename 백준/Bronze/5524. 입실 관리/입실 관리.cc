406442e2d3391579160e848b690d66beb0c23d4c
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2022-10-31 12:54:08 +0900

    [Bronze IV] Title: 입실 관리, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+	cin >> n;
+    
+    for (int i=0; i<n; i++) {
+        string str;
+        cin >> str;
+        
+        for (int j=0; j<str.length(); j++)
+            str[j] = tolower(str[j]);
+        
+        cout << str << "\n";
+    }
+}
