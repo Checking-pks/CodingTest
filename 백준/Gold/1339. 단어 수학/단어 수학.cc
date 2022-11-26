52c4bd4c7c07ee4a7ba95ccc3630fbd894e80a88
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2022-11-26 15:10:30 +0900

    [Gold IV] Title: 단어 수학, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.cc
+++ 백준/Gold/1339. 단어 수학/단어 수학.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <map>
 #include <list>
-#include <vector>
 #include <string>
 #include <cmath>
 
@@ -10,7 +9,6 @@ using namespace std;
 int main() {
 	map<char, int> alphabet;
 	list<pair<char, int>> sortAlphabetValue;
-	vector<string> words;
 	
 	int num, alphabetValue = 9, result = 0;
 	cin >> num;
@@ -19,8 +17,6 @@ int main() {
 	for (int i=0; i<num; i++) {
 		string nowStr;
 		cin >> nowStr;
-
-		words.push_back(nowStr);
 
 		// alphabet에 각 char의 십의 자리 값 저장
 		for (int j=0, length = nowStr.length(); j<length; j++) 
@@ -36,16 +32,9 @@ int main() {
 		return a.second > b.second;
 	});
 
-	// 각 char마다 0~9를 재할당
+	// 값이 큰 순으로 0~9를 곱하여 result 더함
 	for (pair<char, int> value:sortAlphabetValue) 
-		alphabet[value.first] = alphabetValue--;
-
-	// 각 char마다 값 할당 후 합 결과 저장
-	for (string nowStr:words) {
-		for (int j=0, length = nowStr.length(); j<length; j++) {
-			result += alphabet[nowStr[j]] * pow(10, length - j - 1);
-		}
-	}
+		result += value.second * alphabetValue--;
 
 	// output
 	cout << result;
