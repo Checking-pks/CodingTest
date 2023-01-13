5d6f063697f1f4b730cd75f3a6c2a95b49ec1d8e
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-01-13 19:54:34 +0900

    [Bronze IV] Title: Squares, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int area;
+	cin >> area;
+	cout << "The largest square has side length " << int(sqrt(area)) << ".";
+}
