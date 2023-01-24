760cc7bc7bc723ca5bdaa1e87d7a3e4ea8e23610
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-01-24 10:49:20 +0900

    [Bronze IV] Title: A Simple Problem., Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int t;
+	cin >> t;
+
+	for (int i=0; i<t; i++) {
+		long long n;
+		cin >> n;
+		cout << n * n << "\n";
+	}
+}
