59473cd587691c6b11afbbeb6e8890608933b277
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-02-10 09:44:23 +0900

    [Bronze IV] Title: SMS from MCHS, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.cc
+++ 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.cc
@@ -1,20 +1,10 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-	int todayTemperature, todayWindSpeed;
-	int tomorrowTemperature, tomorrowWindSpeed;
-
-	cin >> todayTemperature >> todayWindSpeed;
-	cin >> tomorrowTemperature >> tomorrowWindSpeed;
-
-	if (tomorrowTemperature < 0 && tomorrowWindSpeed >= 10)
-		cout << "A storm warning for tomorrow! Be careful and stay home if possible!";
-	else if (todayTemperature > tomorrowTemperature)
-		cout << "MCHS warns! Low temperature is expected tomorrow.";
-	else if (todayWindSpeed < tomorrowWindSpeed)
-		cout << "MCHS warns! Strong wind is expected tomorrow.";
-	else
-		cout << "No message";
+	int t1, w1, t2, w2;
+	cin >> t1 >> w1 >> t2 >> w2;
+	if (t2 < 0 && w2 >= 10) cout << "A storm warning for tomorrow! Be careful and stay home if possible!";
+	else if (t1 > t2) cout << "MCHS warns! Low temperature is expected tomorrow.";
+	else if (w1 < w2) cout << "MCHS warns! Strong wind is expected tomorrow.";
+	else cout << "No message";
 }
