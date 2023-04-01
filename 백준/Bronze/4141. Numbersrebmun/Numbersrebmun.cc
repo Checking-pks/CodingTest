f5a705f69ce955ef85b517111f16689e3b0932a2
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-04-01 14:51:16 +0900

    [Bronze II] Title: Numbersrebmun, Time: 40 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/4141. Numbersrebmun/Numbersrebmun.cc
+++ 백준/Bronze/4141. Numbersrebmun/Numbersrebmun.cc
@@ -9,17 +9,15 @@ int main() {
 
   for (int i = 0; i < tc; i++) {
     bool p = true;
-    string str;
-    cin >> str;
+    string s;
+    cin >> s;
 
-    for (int j = 0; j < str.length(); j++) {
-      if (n[toupper(str[j]) - 'A'] !=
-          n[toupper(str[str.length() - 1 - j]) - 'A']) {
+    for (int j = 0; j < s.length(); j++) {
+      if (n[toupper(s[j]) - 'A'] != n[toupper(s[s.length() - 1 - j]) - 'A']) {
         p = false;
         break;
       }
     }
-
     cout << (p ? "YES\n" : "NO\n");
   }
 }
