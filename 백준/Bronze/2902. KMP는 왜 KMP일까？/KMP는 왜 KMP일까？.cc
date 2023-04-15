f16e2b6cb7a24250929071822e186c99549e75a7
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-04-15 11:53:27 +0900

    [Bronze II] Title: KMP는 왜 KMP일까?, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    
+    cout << str[0];
+    
+    for (int i=0; i<str.length(); i++) {
+        if (str[i] == '-')
+            cout << str[i+1];
+    }
+}
