a671f4a32055f04b163e891d9ba4a5f24af796a5
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-05-02 13:41:36 +0900

    [Bronze III] Title: 완전 세제곱, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	for (int a=1; a<=100; a++) {
+		for (int b=2; b<=100; b++) {
+			for (int c=b; c<=100; c++) {
+				for (int d=c; d<=100; d++) {
+					if (a * a * a == b * b * b + c * c * c + d * d * d) {
+						cout << "Cube = " << a << ", Triple = (" << b << "," << c << "," << d << ")\n";
+					}
+				}
+			}
+		}
+	}
+}
