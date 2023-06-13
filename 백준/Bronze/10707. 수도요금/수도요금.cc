688b1059c93ac307c9ebfbb1b69389324cefa9b1
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-06-13 12:45:16 +0900

    [Bronze III] Title: 수도요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10707. 수도요금/수도요금.cc
+++ 백준/Bronze/10707. 수도요금/수도요금.cc
@@ -1,9 +1,2 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	int x, yB, yL, yA, u;
-	cin >> x >> yB >> yL >> yA >> u;
-	cout << min(x * u, yB + (u > yL) * (u - yL) * yA);
-}
+#include<iostream>
+int main(){int a,b,c,d,e;std::cin>>a>>b>>c>>d>>e;std::cout<<std::min(a*e,b+(e>c)*(e-c)*d);}
