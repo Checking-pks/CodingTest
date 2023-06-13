9b0eb16e535a1a42578f7cf70ff0277add3ef18d
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-06-13 12:40:51 +0900

    [Bronze III] Title: 수도요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10707. 수도요금/수도요금.cc
+++ 백준/Bronze/10707. 수도요금/수도요금.cc
@@ -3,29 +3,7 @@
 using namespace std;
 
 int main() {
-	// 한달 간 각 회사의 요금
-	int xPriceMonth, yPriceMonth;
-
-	// x 회사의 리터 당 요금
-	int xPricePerLiter;
-
-	// y 회사의 기본 요금, 상한 용량, 추가 요금
-	int yPriceBasic, yLimitBasic, yPriceAdded;
-
-	// 한 달 수도 사용량
-	int useWaterPerMonth;
-
-	// input
-	cin >> xPricePerLiter;
-	cin >> yPriceBasic >> yLimitBasic >> yPriceAdded;
-	cin >> useWaterPerMonth;
-
-	// x 회사 요금 = 리터 당 요금 * 사용량
-	xPriceMonth = xPricePerLiter * useWaterPerMonth;
-	
-	// y 회사 요금 = 기본 요금 + 초과 용량 * 추가 요금
-	yPriceMonth = yPriceBasic + (useWaterPerMonth > yLimitBasic) * (useWaterPerMonth - yLimitBasic) * yPriceAdded;
-
-	// 둘 중 낮은 가격 출력
-	cout << min(xPriceMonth, yPriceMonth);
+	int x, yB, yL, yA, u;
+	cin >> x >> yB >> yL >> yA >> u;
+	cout << min(x * u, yB + (u > yL) * (u - yL) * yA);
 }
