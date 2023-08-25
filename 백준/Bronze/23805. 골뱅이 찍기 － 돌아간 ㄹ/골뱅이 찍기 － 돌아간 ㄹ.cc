c4808f8b5bd9c687976cacefe0396eb03b26760a
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-08-25 11:42:48 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+void print_type1(int num)
+{
+	for (int i=0; i<num; i++) 
+	{
+		for (int j=0; j<num*3; j++) cout << "@";
+		for (int j=0; j<num; j++) cout << " ";
+		for (int j=0; j<num; j++) cout << "@";
+
+		cout << "\n";
+	}
+}
+
+void print_type2(int num)
+{
+	for (int i=0; i<num*3; i++) 
+	{
+		for (int j=0; j<num; j++) cout << "@";
+		for (int j=0; j<num; j++) cout << " ";
+		for (int j=0; j<num; j++) cout << "@";
+		for (int j=0; j<num; j++) cout << " ";
+		for (int j=0; j<num; j++) cout << "@";
+
+		cout << "\n";
+	}
+}
+
+void print_type3(int num)
+{
+	for (int i=0; i<num; i++) 
+	{
+		for (int j=0; j<num; j++) cout << "@";
+		for (int j=0; j<num; j++) cout << " ";
+		for (int j=0; j<num*3; j++) cout << "@";
+
+		cout << "\n";
+	}
+}
+
+int main() {
+	int num;
+	cin >> num;
+
+	print_type1(num);
+	print_type2(num);
+	print_type3(num);
+}
