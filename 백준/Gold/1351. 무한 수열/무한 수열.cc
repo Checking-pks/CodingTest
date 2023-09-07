cdd235d00743f2addd159273bd66801bb76f51d2
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-09-07 13:31:35 +0900

    [Gold V] Title: 무한 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/1351. 무한 수열/무한 수열.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <list>
+#include <map>
+
+using namespace std;
+
+int main() {
+	long long n, p, q, result = 0;
+	cin >> n >> p >> q;
+	
+	list<long long> bucket(1, n);
+	map<long long, long long> numList;
+
+	while (bucket.size()) {
+		long long now = bucket.front();
+		bucket.pop_front();
+
+		if (numList.find(now) != numList.end()) {
+			continue;
+		}
+
+		numList[now] = 0;
+
+		bucket.push_back(now/p);
+		bucket.push_back(now/q);
+	}
+
+	for (pair<long long, long long> now:numList) {
+		if (now.first == 0) {
+			numList[now.first] = 1;
+			continue;
+		}
+
+		numList[now.first] = 
+			numList[now.first / p] + 
+			numList[now.first / q];
+	}
+
+	cout << numList[n];
+}
+
