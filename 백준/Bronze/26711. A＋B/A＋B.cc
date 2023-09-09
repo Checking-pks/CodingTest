c08d5ee129c500fec7030342b3bc56380a9451d9
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-09-09 19:45:08 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Bronze/26711. A＋B/A＋B.cc
+++ 백준/Bronze/26711. A＋B/A＋B.cc
@@ -3,21 +3,20 @@
 using namespace std;
 
 int main() {
-	// input
-	int now = 0, l = 1000000000;
+	int now = 0, boundary = 1000000000;
 	string n1, n2, result = "";
 	cin >> n1 >> n2;
 
-	while (n1.length() % 9) n1 = '0' + n1;
-	while (n2.length() % 9) n2 = '0' + n2;
+	int length = max(n1.length(), n2.length());
+	while (length%9) length++;
 
-	while (n1.length() < n2.length()) n1 = '0' + n1;
-	while (n1.length() > n2.length()) n2 = '0' + n2;
+	while (n1.length() < length) n1 = '0' + n1;
+	while (n2.length() < length) n2 = '0' + n2;
 
 	for (int i=n1.length() - 9; i>=0; i -= 9) {
 		now += stoi(n1.substr(i, 9)) + stoi(n2.substr(i, 9));
-		string nowStr = to_string(now % l);
-		now /= l;
+		string nowStr = to_string(now % boundary);
+		now /= boundary;
 
 		while ((now || i>0) && nowStr.length() < 9)
 			nowStr = '0' + nowStr;
