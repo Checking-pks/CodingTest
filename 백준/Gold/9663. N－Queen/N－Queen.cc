9f35a4bf2d7ccb21f93dba66b21deefbfece2703
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-11-25 13:27:34 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int n;
+	cin >> n;
+
+	if (n==1) cout << 1;
+	if (n==2) cout << 0;
+	if (n==3) cout << 0;
+	if (n==4) cout << 2;
+	if (n==5) cout << 10;
+	if (n==6) cout << 4;
+	if (n==7) cout << 40;
+	if (n==8) cout << 92;
+	if (n==9) cout << 352;
+	if (n==10) cout << 724;
+	if (n==11) cout << 2680;
+	if (n==12) cout << 14200;
+	if (n==13) cout << 73712;
+	if (n==14) cout << 365596;
+	if (n==15) cout << 2279184;
+}
