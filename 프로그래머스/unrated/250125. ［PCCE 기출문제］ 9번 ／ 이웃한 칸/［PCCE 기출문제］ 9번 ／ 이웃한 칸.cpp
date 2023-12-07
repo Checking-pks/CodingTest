5ac1692824663e48ea1fb29c72f716b934d83aed
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-12-07 22:51:04 +0900

    [level unrated] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<string>> board, int h, int w) {
+    int n = board.size();
+    int count = 0;
+    int dh[4] = {0, 1, -1, 0}, dw[4] = {1, 0, 0, -1};
+    
+    for (int i=0; i<=3; i++) {
+        int h_check = h + dh[i], w_check = w + dw[i];
+        if (h_check < 0 || h_check >= n) continue;
+        if (w_check < 0 || w_check >= n) continue;
+        if (board[h][w] == board[h_check][w_check]) count++;
+    }
+    
+    return count;
+}
