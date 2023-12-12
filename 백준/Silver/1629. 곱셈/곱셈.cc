cc102e7ebe9e0b6d61f10027feca990141db1307
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-12-12 13:22:38 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -9,9 +9,8 @@ int main() {
 
 	vector<long long> numList(31);
 	numList[0] = A % C;
-	for (int i=0; i<30; i++) {
+	for (int i=0; i<30; i++) 
 		numList[i+1] = (numList[i] * numList[i]) % C;
-	}
 
 	for (long long i=1, pos=0; i<=B; i<<=1,pos++) {
 		if (B & i) {
