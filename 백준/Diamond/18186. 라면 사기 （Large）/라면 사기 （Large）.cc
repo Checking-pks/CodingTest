421a369dda7169fec245eca39aa854e6bc101e40
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-12-12 13:20:29 +0900

    [Diamond IV] Title: 라면 사기 (Large), Time: 456 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Diamond/18186. 라면 사기 （Large）/라면 사기 （Large）.cc
+++ 백준/Diamond/18186. 라면 사기 （Large）/라면 사기 （Large）.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-	long long N, B, C, result=0, sum=0;
+	long long N, B, C, sum=0, result=0;
 	cin >> N >> B >> C;
 
 	vector<long long> numList(N+2);
@@ -13,31 +13,26 @@ int main() {
 		sum += numList[i];
 	}
 
-	if (B > C) {
-		for (long long i=0, two, three; i<N; i++) {
-			if (numList[i+1] > numList[i+2]) {
-				two = min(numList[i], numList[i+1] - numList[i+2]);
-				numList[i]   -= two;
-				numList[i+1] -= two;
-				result += two * (B+C);
+	if (B <= C) {
+		cout << sum * B;
+		return 0;
+	}
 
-				three = min(numList[i], min(numList[i+1], numList[i+2]));
-				numList[i]   -= three;
-				numList[i+1] -= three;
-				numList[i+2] -= three;
-				result += three * (B+2*C);
-			} else {
-				three = min(numList[i], min(numList[i+1], numList[i+2]));
-				numList[i]   -= three;
-				numList[i+1] -= three;
-				numList[i+2] -= three;
-				result += three * (B+2*C);
-			}
+	for (long long i=0; i<N; i++) {
+		if (numList[i+1] > numList[i+2]) {
+			long long two = min(numList[i], numList[i+1] - numList[i+2]);
+			numList[i]   -= two;
+			numList[i+1] -= two;
+			result += two * (B + C);
+		} 
 
-			result += numList[i] * B;
-		}
-	} else {
-		result = sum * B;
+		long long three = min(numList[i], min(numList[i+1], numList[i+2]));
+		numList[i]   -= three;
+		numList[i+1] -= three;
+		numList[i+2] -= three;
+		result += three * (B + 2*C);
+
+		result += numList[i] * B;
 	}
 
 	cout << result;
