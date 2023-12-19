29336615a25e5fbb76177c58c44960a04c28837f
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2023-12-19 17:09:10 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cc
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cc
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
+unordered_map<int, int> numList;
+
 int dp(int have, int year) {
+	if (numList.find(year) != numList.end()) return numList[year];
 	if (year < 0) return 0;
 	if (year == 0) return have;
-	
-	return max(dp(have, year-1)*105/100, max(dp(have, year-3)*120/100, dp(have, year-5)*135/100));
+
+	int now = max(dp(have, year-1)*105/100, max(dp(have, year-3)*120/100, dp(have, year-5)*135/100));
+	numList[year] = now;
+	return now;
 }
 
 int main() {
