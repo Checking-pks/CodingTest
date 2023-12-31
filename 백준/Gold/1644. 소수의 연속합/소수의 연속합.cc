28571c117eebf0e4c5895e38cf39aeae45b66dde
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2024-01-01 06:18:02 +0900

    [Gold III] Title: 소수의 연속합, Time: 36 ms, Memory: 5596 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -6,12 +6,11 @@ using namespace std;
 vector<int> SieveOfEratosthenes(int num) {
 	vector<int> result;
 	vector<bool> pno(num+1, true);
-	for (int i = 2; i*i<= num; i++) 
-		if (pno[i] == true) 
-			for (int j = i*2; j<= num; j += i) 
-				pno[j] = false;
-	
-	for (int i = 2; i <= num; i++)
+	for (int i=2; i*i<=num; i++) 
+		for (int j=i*2; pno[i] && j<=num; j+=i) 
+			pno[j] = false;
+
+	for (int i=2; i<=num; i++) 
 		if (pno[i])
 			result.push_back(i);
 
@@ -21,14 +20,13 @@ vector<int> SieveOfEratosthenes(int num) {
 int main() {
 	int num, result=0;
 	cin >> num;
-	
+
 	vector<int> primeList = SieveOfEratosthenes(num);
 
 	for (int i=0; i<primeList.size(); i++) {
-		for (int j=i, now=0; j<primeList.size(); j++) {
+		for (int j=i, now=0; now<=num && j<primeList.size(); j++) {
 			now += primeList[j];
 			result += (now == num);
-			if (now > num) break;
 		}
 	}
 
