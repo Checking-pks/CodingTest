7111febf738016364752c107b3925f0a17a358c2
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2024-02-23 08:35:47 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+string s[] = {"12 1600", "11 894", "11 1327", "10 1311", "9 1004", "9 1178", "9 1357", "8 837", "7 1055", "6 556", "6 773"};
+
+int main() {
+	int N;
+	cin >> N;
+	cout << s[N-1];
+}
