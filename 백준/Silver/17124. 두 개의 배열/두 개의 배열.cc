e7330ec392633dc6d64e18fa89f8e0ace40b7f2d
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2024-06-06 17:40:10 +0900

    [Silver III] Title: 두 개의 배열, Time: 1092 ms, Memory: 6284 KB -BaekjoonHub

---
+++ 백준/Silver/17124. 두 개의 배열/두 개의 배열.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	ll T;
+	cin >> T;
+
+	while (T--) {
+		ll n, m, result = 0;
+		cin >> n >> m;
+
+		vector<ll> nList(n), mList(m);
+
+		for (ll i=0; i<n; i++)
+			cin >> nList[i];
+		for (ll i=0; i<m; i++)
+			cin >> mList[i];
+
+		mList.push_back(-1'000'000'001);
+		mList.push_back(1'000'000'001);
+		sort(mList.begin(), mList.end());
+
+		for (ll i=0; i<n; i++) {
+			ll now = nList[i];
+			
+			auto idx = lower_bound(
+						mList.begin(), 
+						mList.end(),
+						now);
+
+			ll left = *(idx - 1), right = *idx;
+
+			if (abs(left - now) <= abs(right - now))
+				result += left;
+			else
+				result += right;
+		}
+
+		cout << result << "\n";
+	}
+}
