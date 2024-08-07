44b0a985b9e7bea263390a5e889d8afc90e284c6
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2024-08-07 11:07:55 +0900

    [Bronze I] Title: 알프수, Time: 4 ms, Memory: 2268 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/23886. 알프수/알프수.cc
+++ 백준/Bronze/23886. 알프수/알프수.cc
@@ -8,9 +8,27 @@ bool isAlpsoo(string X) {
 	if (X[0] > X[1] || X[len - 2] < X[len - 1])
 		return false;
 
-	for (int i = 0; i < len - 1; i++)
-		if (X[i] == X[i + 1])
+	for (int i = 0, slope = 0; i < len - 1; i++) {
+		int nowSlope = X[i + 1] - X[i];
+
+		if (!nowSlope)
 			return false;
+
+		if (!slope) {
+			slope = nowSlope;
+			continue;
+		}
+
+		if (slope > 0 && nowSlope < 0 ||
+			slope < 0 && nowSlope > 0) {
+
+			slope = nowSlope;
+			continue;
+		}
+
+		if (slope != nowSlope)
+			return false;
+	}
 
 	return true;
 }
