6d42ee2b63426c085b334f8948b1357baf9ba877
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2024-08-07 10:54:45 +0900

    [Bronze I] Title: 3개만!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17288. 3개만！/3개만！.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+map<string, int> countList;
+
+int main() {
+	string S;
+	cin >> S;
+
+	S.push_back('/');
+	
+	int result = 0;
+	string now = "";
+	for (int i=0; i<S.length(); i++) {
+		if (now.empty() || now.back() == char(S[i] - 1)) {
+			now.push_back(S[i]);
+			continue;
+		}
+
+		result += (now.length() == 3);
+		now = S[i];
+	}
+
+	cout << result;
+}
