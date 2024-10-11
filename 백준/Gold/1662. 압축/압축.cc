af7e1c16d63e72239ba277afe95f038833a46eed
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2024-10-12 08:01:32 +0900

    [Gold IV] Title: 압축, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1662. 압축/압축.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+string S;
+int pos = -1;
+
+int solve(int mul) {
+    int result = 0;
+    
+    for (++pos; pos<S.length(); pos++) {
+        if (S[pos] == '(') continue;
+        if (S[pos] == ')') break;
+        
+        if (S[pos + 1] == '(')
+            result += solve(S[pos] - '0');
+        else
+            result++;
+    }
+    
+    return mul * result;
+}
+
+int main()
+{
+    cin >> S;
+    
+    S.push_back(')');
+    
+    cout << solve(1);
+}
