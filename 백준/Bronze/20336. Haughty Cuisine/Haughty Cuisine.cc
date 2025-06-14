cb749c2f65b1b553ad88c867f11ac8875a600651
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2025-06-14 21:06:01 +0900

    [Bronze IV] Title: Haughty Cuisine, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20336. Haughty Cuisine/Haughty Cuisine.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, d;
+    cin >> N >> d;
+    
+    cout << d << "\n";
+    for (int i = 0; i < d; i++)
+    {
+        string s;
+        cin >> s;
+        cout << s << "\n";
+    }
+}
