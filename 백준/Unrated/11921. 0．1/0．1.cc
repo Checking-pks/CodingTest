63cb4fb56d68ddd5e435dceae1eb0f58833735c4
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2025-06-14 20:57:26 +0900

    [Unrated] Title: 0.1, Time: 76 ms, Memory: 50728 KB, Score: 5000000 point -BaekjoonHub

---
+++ 백준/Unrated/11921. 0．1/0．1.cc
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char buffer[50000000];
+    fread(buffer, sizeof(char), 50000000, stdin);
+    
+    int N = 0, i;
+    
+    long long sum = 0;
+    
+    for(i = 0; buffer[i] != '\n'; i++) 
+        N = N*10+buffer[i]-'0';
+    
+    for(int j = 0; j < N; j++) 
+    {
+        int v = 0;
+        for(i++; buffer[i] != '\n'; i++) 
+            v = v*10+buffer[i]-'0';
+        
+        sum += v;
+    }
+    
+    cout << N << '\n' << sum << '\n';
+
+    return 0;
+}
