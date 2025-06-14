67cd55b35717d962ca7928823ed619fc07dd7c2d
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2025-06-14 20:58:51 +0900

    [Bronze IV] Title: Quick Estimates, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    while (N--)
+    {
+        string s;
+        cin >> s;
+        cout << s.length() << "\n";
+    }
+}
