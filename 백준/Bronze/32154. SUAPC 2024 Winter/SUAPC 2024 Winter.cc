e571ccc700f79c3c5cee464e07e2e88701548554
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2025-06-14 21:24:06 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    switch(N) {
+        case 1:
+            cout << "11\nA B C D E F G H J L M";
+            break;
+        case 2:
+            cout << "9\nA C E F G H I L M";
+            break;
+        case 3:
+            cout << "9\nA C E F G H I L M";
+            break;
+        case 4:
+            cout << "9\nA B C E F G H L M";
+            break;
+        case 5:
+            cout << "8\nA C E F G H L M";
+            break;
+        case 6:
+            cout << "8\nA C E F G H L M";
+            break;
+        case 7:
+            cout << "8\nA C E F G H L M";
+            break;
+        case 8:
+            cout << "8\nA C E F G H L M";
+            break;
+        case 9:
+            cout << "8\nA C E F G H L M";
+            break;
+        case 10:
+            cout << "8\nA B C F G H L M";
+            break;
+    }
+}
