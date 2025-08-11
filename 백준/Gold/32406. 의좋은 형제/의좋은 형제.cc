4cddf2c7010a406c69e4e61f41b46301ba1763e3
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2025-08-11 20:14:38 +0900

    [Gold V] Title: 의좋은 형제, Time: 108 ms, Memory: 3588 KB -BaekjoonHub

---
+++ 백준/Gold/32406. 의좋은 형제/의좋은 형제.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    vector<int> a(N), b(N);
+    for (int i = 0; i < N; i++) cin >> a[i];
+    for (int i = 0; i < N; i++) cin >> b[i];
+    
+    int result = abs(a[N - 1] + b[N - 2] - a[N - 2] - b[N - 1]);
+    
+    for (int i = 0; i < N - 2; i++)
+        result += abs(a[i] - b[i]);
+    
+    cout << result;
+}
