360efa1161664df9d82606297ee7cc6abc6b5649
KyeungSoo Park <69112514+Checking-pks@users.noreply.github.com>
2025-10-15 18:39:11 +0900

    [Gold II] Title: 짝수번째 피보나치 수의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/11443. 짝수번째 피보나치 수의 합/짝수번째 피보나치 수의 합.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+#define DIV 1000000007;
+typedef long long ll;
+
+unordered_map<ll, ll> record = {{1, 1}, {2, 1}};
+
+ll fib(ll n)
+{
+    if (n <= 0) return 0;
+    if (record.find(n) != record.end())
+        return record[n];
+    
+    ll a = n / 2, b = n / 2 + (n & 1);
+    ll result = fib(b) * fib(a + 1) + fib(b - 1) * fib(a);
+    result %= DIV;
+
+    return record[n] = result;
+}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    cout << fib(N + (N % 2 == 0)) - 1;
+}
